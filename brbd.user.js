c376fe5a287e8aaa43d612b7e340a1aee8393406
ciberweaboo <54245455+ciberweaboo@users.noreply.github.com>
2023-09-19 00:15:45 +0200

    Create brbd.user.js

---
+++ brbd.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         BCX Reset Button Disabler Loader
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  You know what is this addon for.
+// @author       weboos
+// @match        https://bondage-europe.com/R96/BondageClub/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bondage-europe.com
+// @grant        none
+// ==/UserScript==
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+var code = httpGet("http://127.0.0.1:5500/brbd.js")
+const script= document.createElement("script")
+script.type = "text/javascript"
+script.innerHTML = code
+document.head.appendChild(script)
+eval(script)
