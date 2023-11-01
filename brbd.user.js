f3a481888a38852e34468796df5f34c3e7aae562
dDeepLb <71733861+dDeepLb@users.noreply.github.com>
2023-11-01 04:25:24 +0200

    Replace overcomplicated code... Again

--- brbd.user.js
+++ brbd.user.js
@@ -10,17 +10,8 @@
 // @grant        none
 // ==/UserScript==
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
-
-var code = httpGet("https://raw.githubusercontent.com/ciberweaboo/brbd/main/brbd.js")
-const script= document.createElement("script")
-script.type = "text/javascript"
-script.innerHTML = code
-document.head.appendChild(script)
-eval(script)
+(function () {
+    "use strict";
+    
+    javascript:void function(){function httpGet(a){var b=new XMLHttpRequest;return b.open("GET",a,!1),b.send(null),b.responseText}var code=httpGet("https://raw.githubusercontent.com/ciberweaboo/brbd/main/brbd.js");const script=document.createElement("script");script.type="text/javascript",script.innerHTML=code,document.head.appendChild(script),eval(script)}();
+})();
